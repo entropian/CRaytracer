876f8ef113f474304c8540116b53ed9300bf96c6
Bowen Yan <KH2010G@gmail.com>
2016-09-17 03:33:26 -0500

    Added ppm write

---
+++ imagefile.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <stdio.h>
+
+int PPM_write(const char *file_name, const unsigned char *image, const int image_size, 
+			   const int width, const int height)
+{
+	int num_pixel = width * height;	
+	if (image_size / 3 != num_pixel)
+	{
+		fprintf(stderr, "Image size doesn't match image dimensions.\n");
+		return -1;
+	}
+
+	FILE *fp;
+	fopen_s(&fp, file_name, "wb");
+
+	// Header
+	fprintf(fp, "P6\n");
+	fprintf(fp, "512 512\n");
+	fprintf(fp, "255\n");
+	
+	fwrite(image, sizeof(char), image_size, fp);
+	fclose(fp);
+	return 0;
+}
